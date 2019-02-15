0c6e768cb6d9bdf18ebf1a688b93fb737e93bf98
Maciej  Smolarz <smola11@o2.pl>
2019-02-16 00:19:56 +0100

    Added Project domain class

---
+++ src/main/java/com/maciej/ppmtool/domain/Project.java
@@ -0,0 +1,39 @@
+package com.maciej.ppmtool.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Setter
+@Getter
+@Entity
+public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String projectName;
+    private String projectIdentifier;
+    private String desciption;
+    private Date stat_date;
+    private Date end_date;
+    private Date created_at;
+    private Date updated_at;
+
+    public Project() {
+    }
+
+    @PrePersist
+    protected void onCreate() {
+        this.created_at = new Date();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updated_at = new Date();
+    }
+
+
+}
