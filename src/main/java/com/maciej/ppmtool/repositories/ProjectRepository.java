2369929fb3d0abc5231143efaf4f636e112a3117
Maciej  Smolarz <smola11@o2.pl>
2019-02-16 00:26:13 +0100

    ProjectRepository added

---
+++ src/main/java/com/maciej/ppmtool/repositories/ProjectRepository.java
@@ -0,0 +1,10 @@
+package com.maciej.ppmtool.repositories;
+
+import com.maciej.ppmtool.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+    
+}
